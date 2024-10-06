98f4a675cd86cdb0f8f7629ec26a62852819fa4d
Nik <stalker3717@gmail.com>
2024-10-06 13:14:00 +0300

    comments
    did we need those

--- parse.py
+++ parse.py
@@ -9,6 +9,8 @@ import os
 botcsv = open("bots.csv", "r")
 config = tomllib.load(open("config.toml", "rb"))
 mode = config['script']['tag']
+
+# warn user of possibly mistyped settings
 if mode.lower() not in ['clan', 'owner', 'none']:
     print(f"WARNING: Clan tag mode is currently set to '{mode}'."
           " The script will ignore this setting, default to 'none' and won't give bots clan tags."
@@ -20,13 +22,18 @@ if 'clan' not in clanstr.lower():
           " The script will ignore this setting and won't give bots ADAPTED clan tags."
           f"\nPress any key to proceed, or manually close the window to interrupt.")
     input()
+
+# get rid of \n's from the file
 botlines = botcsv.readlines()
 botlines = [w.replace('\n', '') for w in botlines]
+
 botstxt = open("bots.txt", "w")
-informed = False
+informed = False  # this is for the warning just below
 for bot in botlines:
     botinfo = bot.split(',')
     if len(botinfo[0]) >= 15 and informed is False:
+        # by the way games without clan tags don't have this problem i think?
+        # CoD4 doesn't at least
         print(f"WARNING: Name '{botinfo[0]}' is {len(botinfo[0])} characters long"
               f" and may not display correctly in-game."
               f"\nModern Warfare 2 & 3 cut names off at 15 characters;"
@@ -42,11 +49,14 @@ for bot in botlines:
     else:
         line = botinfo[0]
     botstxt.write(line + "\n")
+
+# make a directory if it doesn't exist already
 try:
     botsnctxt = open("noclan/bots.txt", "w")
 except FileNotFoundError:
     os.makedirs('noclan')
     botsnctxt = open("noclan/bots.txt", "w")
+
 for bot in botlines:
     botinfo = bot.split(',')
     if "clan" in clanstr.lower():
